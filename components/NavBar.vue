4f914646080a45869cf7da189b321424847fa7df
VinzOo93 <v.orru@live.fr>
2021-03-13 13:09:08 +0100

    NAVBAR ANIM OK

---
+++ components/NavBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class='navbar'>
+    <nav>
+      <ul>
+      <li><nuxt-link class='navbar-link' to='#'>Developpement Web</nuxt-link> </li>
+      <li><nuxt-link class='navbar-link' to='#'>Galerie Photo</nuxt-link></li>
+      <li><nuxt-link class='navbar-link' to='#'>A propos de moi</nuxt-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+
+.navbar {
+  padding: 2rem 3rem;
+  margin-bottom: 3rem;
+  text-align: center;
+  font-weight: bold;
+  font-size: 25px;
+}
+
+ul {
+  display: flex;
+  justify-content: flex-end;
+}
+
+li{
+  position: relative;
+  list-style: none;
+  margin-right: 25px;
+}
+a.navbar-link {
+  position: relative;
+}
+
+a.navbar-link:before {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 3px;
+  bottom: 0;
+  left: 0;
+  background-color: black;
+  transform: scaleX(0);
+  font-weight: bold;
+  transition: all 0.3s ease-in-out;
+
+}
+
+a.navbar-link:hover:before {
+  visibility: visible;
+  transform: scaleX(1);
+
+  color: black;
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,16 @@
 <template>
   <div>
-    <Nuxt />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@600&display=swap" rel="stylesheet">
+    <NavBar />
   </div>
 </template>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+* {
+  font-family: 'Cormorant Garamond', serif;
   text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  color: #504e4c;
 }
 </style>
