17a5344caca950120fff49f64e8318c8ecff7c70
VinzOo93 <v.orru@live.fr>
2021-03-16 13:40:40 +0100

    galerie mise en page grand écran ok

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,6 +12,20 @@
   text-decoration: none;
   color: #504e4c;
 }
+
+body {
+  margin: 0;
+  width: 100vw;
+  line-height: 1.5;
+  text-rendering: optimizeSpeed;
+  -webkit-text-size-adjust: 100%;
+  -moz-text-size-adjust: 100%;
+  scroll-behavior: smooth;
+  overscroll-behavior-y: none;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+
 </style>
 <script>
 
--- pages/photo/index.vue
+++ pages/photo/index.vue
@@ -1,39 +1,37 @@
 <template>
 
-  <div class='album-header-sticky'>
-    <div class='container'>
-      <div class='content'>
-        <h1 class='heading is-large album-header-heading js-heading'>
+  <div class='galery'>
+    <div class='album-header-sticky'>
+      <div class='container'>
+        <div class='content'>
+          <h1 class='heading is-large album-header-heading js-heading'>
 
-          <div>Tasmania</div>
+            <div>Tasmania</div>
 
-          <div> Four Mile Creek</div>
+            <div> Four Mile Creek</div>
 
-        </h1>
+          </h1>
 
-        <h2> Ces images ont été capturée vers la fin de mon aventure en Tasmanie. Cette île au sud de l'Australie est
-          l'un des endroits où le ciel est l'un des plus clair au monde. Gràce à sa position géographique particulière, nous pouvons y admirer occasionellement la lueur orangée des aurores australes.</h2>
-        <h2> Pour l'oeil le plus averti, vous pouvez observer des lignes dans le ciel qui sont des météorites en chute.</h2>
+          <h3> Ces images ont été capturée vers la fin de mon aventure en Tasmanie. Cette île au sud de l'Australie est
+            l'un des endroits où le ciel est l'un des plus clair au monde. Gràce à sa position géographique
+            particulière, nous pouvons y admirer occasionellement la lueur orangée des aurores australes.</h3>
+          <h3> Pour l'oeil le plus averti, vous pouvez observer des lignes dans le ciel qui sont des météorites en
+            chute.</h3>
 
-        <img class='cover2 js-img' alt='second-image' src='~/assets/images/5.JPG'>
+          <img class='cover2 js-img' alt='second-image' src='~/assets/images/5.JPG'>
+          <img class='cover2 js-img' alt='second-image' src='~/assets/images/17.JPG'>
 
+        </div>
+      </div>
+      <div class='container'>
+        <img class='cover js-img' alt='first-image' src='~/assets/images/aurora.jpg'>
+      </div>
+      <div class='container'>
+
+        <img class='cover3 js-img' alt='second-image' src='~/assets/images/1.JPG'>
       </div>
     </div>
-    <div class='container'>
-      <img class='cover js-img' alt='first-image' src='~/assets/images/aurora.jpg'>
-    </div>
-    <div class='container'>
-      <p class='title-content'>"Qui veille la nuit se rejouit le matin"</p>
-      <img class='cover2 js-img' alt='second-image' src='~/assets/images/1.JPG'>
-
-
-      <img class='cover3 js-img' alt='second-image' src='~/assets/images/17.JPG'>
-
-    </div>
-
   </div>
-
-
 
 
 </template>
@@ -49,7 +47,9 @@ export default {
 img {
   display: block;
   max-width: 100%;
+  max-height: 100%;
 }
+
 
 .container {
   display: grid;
@@ -62,40 +62,35 @@ img {
 
 @media only screen and  (min-width: 768px) {
   .cover:first-child {
-    grid-column: 2/12;
+    grid-column: 2/8;
     z-index: 1;
+    max-width: 95%;
   }
 
   .cover2 {
-    grid-column: 2/12;
-    z-index: 1;
-    display: block;
-    max-width: 100%;
-  }
-  .cover3  {
-    grid-column: 6/12;
+    padding-top: 25px;
+    grid-column: 2/8;
     z-index: 1;
     max-width: 100%;
   }
+
   .content {
-    grid-column: 7/12;
+    grid-column: 8/12;
     position: absolute;
     width: 100%;
   }
-  .title-content {
-    grid-column: 9/10;
-  }
-}
 
-.title-content {
-  font-size: 30px;
-  position: absolute;
-  top: 150px;
-  white-space: nowrap;
+  .cover3 {
+    grid-column: 4/12;
+    z-index: 1;
+    max-width: 100%;
+  }
+
 }
 
 .cover3 {
-  margin-top: -100px;
+  position: relative;
+  top: 30px;
 }
 
 
