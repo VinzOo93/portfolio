bcd93f13432a25a90bb65bb9d59558d654b3ed7f
VinzOo93 <v.orru@live.fr>
2021-03-17 16:22:31 +0100

    navigation  okk

--- pages/photo/index.vue
+++ pages/photo/index.vue
@@ -4,31 +4,19 @@
     <div class='album-header-sticky'>
       <div class='container'>
         <div class='content'>
-          <h1 class='heading is-large album-header-heading js-heading'>
-
-            <div>Tasmania</div>
-
-            <div> Four Mile Creek</div>
-
-          </h1>
-
-          <h3> Ces images ont été capturée vers la fin de mon aventure en Tasmanie. Cette île au sud de l'Australie est
-            l'un des endroits où le ciel est l'un des plus clair au monde. Gràce à sa position géographique
-            particulière, nous pouvons y admirer occasionellement la lueur orangée des aurores australes.</h3>
-          <h3> Pour l'oeil le plus averti, vous pouvez observer des lignes dans le ciel qui sont des météorites en
-            chute.</h3>
-
           <img class='cover2 js-img' alt='second-image' src='~/assets/images/5.JPG'>
           <img class='cover2 js-img' alt='second-image' src='~/assets/images/17.JPG'>
-
+          <img class='cover2 js-img' alt='second-image' src='~/assets/images/1.JPG'>
         </div>
       </div>
       <div class='container'>
+        <div class='cover text-content'>
+          <p class='description'> Ces images ont été capturées vers la fin de mon aventure en Tasmanie. Cette île au sud de l'Australie
+            est l'un des endroits où le ciel est l'un des plus clair au monde.<br><br> Grâce à sa position géographique
+            particulière, nous pouvons y admirer occasionellement la lueur orangée  provenant d'aurores australes.</p>
+        </div>
         <img class='cover js-img' alt='first-image' src='~/assets/images/aurora.jpg'>
-      </div>
-      <div class='container'>
 
-        <img class='cover3 js-img' alt='second-image' src='~/assets/images/1.JPG'>
       </div>
     </div>
   </div>
@@ -57,41 +45,76 @@ img {
   grid-gap: 0;
   align-items: start;
   position: relative;
-  margin-top: 50px;
+  width: 100%;
+
 }
 
-@media only screen and  (min-width: 768px) {
-  .cover:first-child {
-    grid-column: 2/8;
+@media only screen and  (max-width: 768px) {
+
+  .container {
+    display: block;
+    align-items: center;
+    position: relative;
+  }
+
+  .cover {
+    margin: 15px 15px 15px 15px;
     z-index: 1;
-    max-width: 95%;
+    max-width: 90%;
+
   }
 
   .cover2 {
-    padding-top: 25px;
-    grid-column: 2/8;
+    margin: 20px 20px 20px 20px;
     z-index: 1;
-    max-width: 100%;
+    max-width: 90%;
   }
 
   .content {
-    grid-column: 8/12;
-    position: absolute;
+    position: relative;
     width: 100%;
   }
+}
 
-  .cover3 {
-    grid-column: 4/12;
-    z-index: 1;
-    max-width: 100%;
-  }
+  @media only screen and  (min-width: 768px) {
+
+    .container {
+      display: grid;
+      grid-template-columns: repeat(12, 1fr);
+      grid-gap: 0;
+      align-items: start;
+      grid-column: 6/8;
+      position: absolute;
+      width: 100%;
+    }
+
+    .cover {
+      grid-column: 2/7;
+      z-index: 1;
+      max-width: 95%;
+    }
+
+    .cover2 {
+      margin: 40px 10px 10px 20px;
+      grid-column: 2/8;
+      z-index: 1;
+      max-width: 100%;
+    }
+
+    .content {
+      grid-column: 7/12;
+      position: absolute;
+      width: 100%;
+    }
 
 }
 
-.cover3 {
-  position: relative;
-  top: 30px;
+.text-content {
+  display: flex;
+  max-width: 100%;
 }
-
+.description {
+  font-size: 27px;
+}
 
 </style>
