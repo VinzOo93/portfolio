b42d3afb43aaea93099a5c65c233abf5ba9149c5
vincentorru <v.orru@live.fr>
2021-04-20 12:32:37 +0200

    html|css acceuil OK

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,8 @@
         <div class='hero__lineWrap overflow-hidden is-inview'>
           <p class='welcome line p-0 m-0'><span class='customFont'>HEY</span> <img src='https://media.giphy.com/media/VDNDX5BhKKz0YsJkl0/giphy.gif' class='eyes'  alt='eyes'> WELCOME</p>
           <p class='welcome line p-0 m-0'>  <img src='https://media.giphy.com/media/RSgKTx5RRZj34rbbu7/giphy.gif' class='covid' alt='covid'> IN MY <span class='customFont'>WORLD</span><img src='https://media.giphy.com/media/dvgjUJsZ60Wyw8UNyM/giphy.gif' class='world' width='70px'  alt='world'></p>
-          <p class='welcome line p-0 m-0'> 20-<span class='customFont'>CENT</span><img src='https://media.giphy.com/media/f4DxcVLlIOAgsYDJCG/giphy.gif' class='gin'  alt='gin'> 2021 </p>
-          <p class='welcome line p-0 m-0'>(<img src='https://media.giphy.com/media/qNSRPyKLJIzZb0IJnB/giphy.gif' class='lemon' alt='lemon'>)=> <span class='customFont'>ENJOY</span> <=(<img src='https://media.giphy.com/media/MAms0vmRszwHe/giphy.gif' class='strawberry'  alt='strawberry'>)</p>
+          <p class='welcome line p-0 m-0'> 20<img src='~/assets/home/rock.jpg' class='rock' alt='rock'><span class='customFont'>CENT</span><img src='https://media.giphy.com/media/f4DxcVLlIOAgsYDJCG/giphy.gif' class='gin'  alt='gin'> 2021 </p>
+          <p class='welcome line p-0 m-0'>(<img src='https://media.giphy.com/media/qNSRPyKLJIzZb0IJnB/giphy.gif' class='lemon' alt='lemon'>)=><span class='customFont'>ENJOY</span><=(<img src='https://media.giphy.com/media/MAms0vmRszwHe/giphy.gif' class='strawberry'  alt='strawberry'>)</p>
         </div>
       </div>
     </section>
@@ -38,12 +38,23 @@ img {
   image-rendering: auto;
 }
 
+.rock {
+  width: 12.416666667vw;
+  margin-left: 20px;
+  margin-right: 20px;
+
+}
+
 .lemon  {
   height: 6.416666667vw;
 }
 
 .strawberry {
   height: 6.416666667vw;
+}
+
+.world {
+  margin-left: 20px;
 }
 
 
@@ -79,13 +90,13 @@ section {
 }
 
 .welcome {
-  font-size: 9.027777778vw;
+  font-size: 10.027777778vw;
   line-height: .886722663;
 }
 
 .hero__wrap {
-  margin-right: 10%;
-  margin-left: 10%;
+  margin-right: 7%;
+  margin-left: 7%;
   position: relative;
   bottom: 80px;
 
