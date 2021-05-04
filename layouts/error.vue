43727520b5df1e88dbad30de2738a9de19589959
VinzOo93 <v.orru@live.fr>
2021-05-04 16:48:33 +0200

    error.vue
    Ok

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class='title'>
+      <h1 v-if="error.statusCode === 404">Oups !</h1>
+      <h1 v-else>Une erreur est survenue</h1>
+      <h2>La page que vous recherchez n'éxiste pas</h2>
+      <img src='https://media.giphy.com/media/tNC2rod1uTrdC/giphy.gif' alt='bender'>
+    </div>
+
+    <NuxtLink to="/">Page d'accueil</NuxtLink>
+  </div>
+</template>
+<style scoped>
+* {
+  text-align: center;
+}
+</style>
+<script>
+export default {
+  props: ['error'],
+  layout: 'error' // nous pouvons definir un layout personnalisé pour la page erreur
+}
+</script>
+
