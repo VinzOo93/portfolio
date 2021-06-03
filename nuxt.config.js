8068d1310d83e2806cfb7b28c722105c255afe07
VinzOo93 <v.orru@live.fr>
2021-06-03 21:34:58 +0200

    add object perfect
    analitic ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Vincent ORRU - Développeur Web - Découvrir mes réalisations' },
+      { hid: 'description', name: 'description', content: 'Vincent ORRU - Développeur Web - Découvrir mes réalisations - Projets web - Galerie photo - à propos de moi' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: 'signature.png'},
       {rel: "preconnect", href: "https://fonts.gstatic.com" },
