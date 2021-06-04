9c64e9f7a7104ca6cd7048c4ff99767e8fed9bb7
VinzOo93 <v.orru@live.fr>
2021-06-04 22:09:08 +0200

    tableau score OK

--- components/ModaleScore.vue
+++ components/ModaleScore.vue
@@ -3,6 +3,27 @@
     <div class='overlay-game' v-on:click='toggleModaleScore'></div>
     <div class='modale card'>
       <div class='btn-modale' v-on:click='toggleModaleScore'></div>
+      <h1>Tableau des scores ☑️</h1>
+      <div class='table'>
+        <table class='table-player'>
+          <thead>
+          <tr class='player-line'>
+            <td>Joueurs</td>
+            <td>succès %</td>
+            <td>Tentatives</td>
+            <td>Temps</td>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class='player-line t-body' v-for='Player in Players'>
+            <td> {{ Player.name }}</td>
+            <td> {{ Player.successRate }}%</td>
+            <td>{{ Player.tentative }}</td>
+            <td> {{ Player.timeLeft }}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
 
       <div>
       </div>
@@ -11,11 +32,33 @@
   </div>
 </template>
 <script>
+import * as firebase from 'firebase'
 
-
+let app = firebase.default
+let db = app.database()
+let data
 export default {
   name: 'ModaleScore',
-  props: ['reveleScore', 'toggleModaleScore']
+  props: ['reveleScore', 'toggleModaleScore'],
+
+  data() {
+    return {
+      Players: []
+    }
+  }, async fetch() {
+
+    db.ref().child('Player').get().then((e) => {
+      this.Players = e.val()
+
+      if (e.exists()) {
+        console.log(e.val())
+      } else {
+        console.log('No data available')
+      }
+    }).catch((error) => {
+      console.error(error)
+    })
+  }
 }
 </script>
 <style>
@@ -105,6 +148,52 @@ export default {
   padding: 50px;
   position: fixed;
 }
+
+
+.player-line {
+  display: flex;
+  justify-content: space-evenly;
+
+}
+
+.player-line td {
+  align-items: center;
+}
+
+
+.table-player {
+  width: 80%;
+  height: 80%;
+  margin: 100px;
+  border: solid 1px #74746e;
+  border-radius: 30px;
+}
+
+.t-body * {
+  padding: 10px;
+  margin-top: 30px;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+thead *{
+  font-size: 15px;
+  font-family: Helvetica, serif !important;
+}
+h1 {
+  margin-top: 15%;
+  display: flex;
+  justify-content: center;
+}
+
+.table {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+
 </style>
 
 
